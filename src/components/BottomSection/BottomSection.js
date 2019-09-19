5a2960ba820013a5c84e2eba91d6ebafcc1486eb
Serghiy <shevtsiv.serghiy@gmail.com>
2019-09-19 14:18:42 +0300

    Site structure

--- src/components/BottomSection/BottomSection.js
+++ src/components/BottomSection/BottomSection.js
@@ -74,20 +74,20 @@ export default class BottomSection extends Component {
                         </p>
                     </div>
                 </div>
-                
-                <div className="customers">
-                    <div className="wrap">
-                        <div className="flex">
-                            <div className="image">
-                                <img src="" alt=""/>
-                            </div>
-                            <div className="info">
-                                <strong className="name">Marina Ahsanov</strong>
-                                <div className="earn">just earnd $481.</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+                git pull origin master {/*<div className="customers">*/}
+                {/*    <div className="wrap">*/}
+                {/*        <div className="flex">*/}
+                {/*            <div className="image">*/}
+                {/*                <img src="" alt=""/>*/}
+                {/*            </div>*/}
+                {/*            <div className="info">*/}
+                {/*                <strong className="name">Marina Ahsanov</strong>*/}
+                {/*                <div className="earn">just earnd $481.</div>*/}
+                {/*            </div>*/}
+                {/*        </div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
 
                 <div className={this.state.notificationClass} ref="msg">
                     <div className="wrap">
