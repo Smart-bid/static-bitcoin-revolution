6b8b72c4df752f811e2f5509d4638c6dc527e8a1
Serghiy <shevtsiv.serghiy@gmail.com>
2019-09-25 14:42:16 +0300

    Change path

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
-import LpFramework, {LpFrameworkWrapper} from '../../lp-framework-master/dist/index';
+import LpFramework, {LpFrameworkWrapper} from '../lp-framework-master/dist/index';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
